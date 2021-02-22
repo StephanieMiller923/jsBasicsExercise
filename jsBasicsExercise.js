54052d1243e6be94ff9ccb0830e9b76688305c82
Stephanie Miller <stephanie.miller923@gmail.com>
2021-02-22 12:09:09 -0600

    jsBasicsExercise

---
+++ jsBasicsExercise.js
@@ -0,0 +1,11 @@
+console.log("7");
+// The five main data types/primitives are: Numbers, Strings, Booleans, Null, and Undefined.
+// The 6 operator symbols are: + - * / ** %
+console.log(5%4);
+console.log(5%-4);
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+console.log("Wanda" + "Vision");
+console.log("Stephanie".length + "Miller".length);
+console.log("Avengers: Endgame"[16]);
+console.log("/");
+
